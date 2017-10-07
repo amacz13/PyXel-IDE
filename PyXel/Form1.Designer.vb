54522babd3e7d06238c5f2bd06d6cf3d7646f79a
amacz13 <amacz13@outlook.fr>
2017-10-07 21:10:22 +0200

    Ajoutez des fichiers projet.

---
+++ PyXel/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits ComponentFactory.Krypton.Toolkit.KryptonForm
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.KryptonRibbon1 = New ComponentFactory.Krypton.Ribbon.KryptonRibbon()
+        Me.KryptonRibbonQATButton1 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonQATButton()
+        Me.KryptonRibbonQATButton2 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonQATButton()
+        Me.KryptonContextMenuItem1 = New ComponentFactory.Krypton.Toolkit.KryptonContextMenuItem()
+        Me.KryptonContextMenuItem2 = New ComponentFactory.Krypton.Toolkit.KryptonContextMenuItem()
+        Me.KryptonContextMenuItem3 = New ComponentFactory.Krypton.Toolkit.KryptonContextMenuItem()
+        Me.ButtonSpecAppMenu1 = New ComponentFactory.Krypton.Ribbon.ButtonSpecAppMenu()
+        Me.ButtonSpecAppMenu2 = New ComponentFactory.Krypton.Ribbon.ButtonSpecAppMenu()
+        Me.KryptonRibbonTab1 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonTab()
+        Me.KryptonRibbonGroup1 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup()
+        Me.KryptonRibbonGroupTriple1 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple()
+        Me.KryptonRibbonGroupButton1 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton()
+        Me.KryptonRibbonGroup2 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup()
+        Me.KryptonRibbonGroupTriple2 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple()
+        Me.KryptonRibbonGroupButton4 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton()
+        Me.KryptonRibbonGroupButton5 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton()
+        Me.KryptonRibbonTab2 = New ComponentFactory.Krypton.Ribbon.KryptonRibbonTab()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.FastColoredTextBox1 = New FastColoredTextBoxNS.FastColoredTextBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        CType(Me.KryptonRibbon1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FastColoredTextBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'KryptonRibbon1
+        '
+        Me.KryptonRibbon1.InDesignHelperMode = True
+        Me.KryptonRibbon1.Name = "KryptonRibbon1"
+        Me.KryptonRibbon1.QATButtons.AddRange(New System.ComponentModel.Component() {Me.KryptonRibbonQATButton1, Me.KryptonRibbonQATButton2})
+        Me.KryptonRibbon1.RibbonAppButton.AppButtonMenuItems.AddRange(New ComponentFactory.Krypton.Toolkit.KryptonContextMenuItemBase() {Me.KryptonContextMenuItem1, Me.KryptonContextMenuItem2, Me.KryptonContextMenuItem3})
+        Me.KryptonRibbon1.RibbonAppButton.AppButtonSpecs.AddRange(New ComponentFactory.Krypton.Ribbon.ButtonSpecAppMenu() {Me.ButtonSpecAppMenu1, Me.ButtonSpecAppMenu2})
+        Me.KryptonRibbon1.RibbonAppButton.AppButtonText = "Fichier"
+        Me.KryptonRibbon1.RibbonStrings.CustomizeQuickAccessToolbar = "Personnalisez les raccourics"
+        Me.KryptonRibbon1.RibbonStrings.Minimize = "Réduire le ruban"
+        Me.KryptonRibbon1.RibbonStrings.MoreColors = "&Plus de couleurs..."
+        Me.KryptonRibbon1.RibbonStrings.NoColor = "&Pas de couleurs"
+        Me.KryptonRibbon1.RibbonStrings.RecentColors = "Couleurs récentes"
+        Me.KryptonRibbon1.RibbonStrings.RecentDocuments = "Fichiers récents"
+        Me.KryptonRibbon1.RibbonStrings.ShowAboveRibbon = "&Afficher au dessus du Ruban"
+        Me.KryptonRibbon1.RibbonStrings.ShowBelowRibbon = "&Afficher en dessous du Ruban"
+        Me.KryptonRibbon1.RibbonStrings.ShowQATAboveRibbon = "&Afficher les raccourcis au dessus du Ruban"
+        Me.KryptonRibbon1.RibbonStrings.ShowQATBelowRibbon = "&Afficher les raccourcis en dessous du Ruban"
+        Me.KryptonRibbon1.RibbonTabs.AddRange(New ComponentFactory.Krypton.Ribbon.KryptonRibbonTab() {Me.KryptonRibbonTab1, Me.KryptonRibbonTab2})
+        Me.KryptonRibbon1.SelectedTab = Me.KryptonRibbonTab1
+        Me.KryptonRibbon1.Size = New System.Drawing.Size(1182, 115)
+        Me.KryptonRibbon1.TabIndex = 0
+        '
+        'KryptonRibbonQATButton1
+        '
+        Me.KryptonRibbonQATButton1.Image = Global.PyXel.My.Resources.Resources.save16
+        Me.KryptonRibbonQATButton1.Text = "Enregistrer"
+        '
+        'KryptonRibbonQATButton2
+        '
+        Me.KryptonRibbonQATButton2.Image = Global.PyXel.My.Resources.Resources.run16
+        Me.KryptonRibbonQATButton2.Text = "Executer"
+        '
+        'KryptonContextMenuItem1
+        '
+        Me.KryptonContextMenuItem1.Image = Global.PyXel.My.Resources.Resources.new32
+        Me.KryptonContextMenuItem1.Text = "Nouveau"
+        '
+        'KryptonContextMenuItem2
+        '
+        Me.KryptonContextMenuItem2.Image = Global.PyXel.My.Resources.Resources.open32
+        Me.KryptonContextMenuItem2.Text = "Ouvrir"
+        '
+        'KryptonContextMenuItem3
+        '
+        Me.KryptonContextMenuItem3.Image = Global.PyXel.My.Resources.Resources.save321
+        Me.KryptonContextMenuItem3.Text = "Enregistrer"
+        '
+        'ButtonSpecAppMenu1
+        '
+        Me.ButtonSpecAppMenu1.Image = Global.PyXel.My.Resources.Resources.help
+        Me.ButtonSpecAppMenu1.Text = "A propos"
+        Me.ButtonSpecAppMenu1.UniqueName = "03656E50B816453706A5AE29FD944B66"
+        '
+        'ButtonSpecAppMenu2
+        '
+        Me.ButtonSpecAppMenu2.Image = Global.PyXel.My.Resources.Resources.close
+        Me.ButtonSpecAppMenu2.Text = "Quitter"
+        Me.ButtonSpecAppMenu2.UniqueName = "148F529BABBC45FE508A6EDA6486F2E3"
+        '
+        'KryptonRibbonTab1
+        '
+        Me.KryptonRibbonTab1.Groups.AddRange(New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup() {Me.KryptonRibbonGroup1, Me.KryptonRibbonGroup2})
+        Me.KryptonRibbonTab1.KeyTip = "A"
+        Me.KryptonRibbonTab1.Text = "Accueil"
+        '
+        'KryptonRibbonGroup1
+        '
+        Me.KryptonRibbonGroup1.Items.AddRange(New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupContainer() {Me.KryptonRibbonGroupTriple1})
+        Me.KryptonRibbonGroup1.TextLine1 = "Code"
+        '
+        'KryptonRibbonGroupTriple1
+        '
+        Me.KryptonRibbonGroupTriple1.Items.AddRange(New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupItem() {Me.KryptonRibbonGroupButton1})
+        '
+        'KryptonRibbonGroupButton1
+        '
+        Me.KryptonRibbonGroupButton1.ImageLarge = Global.PyXel.My.Resources.Resources.comment
+        Me.KryptonRibbonGroupButton1.ImageSmall = Global.PyXel.My.Resources.Resources.comment
+        Me.KryptonRibbonGroupButton1.TextLine1 = "Commenter"
+        '
+        'KryptonRibbonGroup2
+        '
+        Me.KryptonRibbonGroup2.Items.AddRange(New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupContainer() {Me.KryptonRibbonGroupTriple2})
+        Me.KryptonRibbonGroup2.TextLine1 = "Execution"
+        '
+        'KryptonRibbonGroupTriple2
+        '
+        Me.KryptonRibbonGroupTriple2.Items.AddRange(New ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupItem() {Me.KryptonRibbonGroupButton4, Me.KryptonRibbonGroupButton5})
+        '
+        'KryptonRibbonGroupButton4
+        '
+        Me.KryptonRibbonGroupButton4.ImageLarge = Global.PyXel.My.Resources.Resources.run
+        Me.KryptonRibbonGroupButton4.ImageSmall = Global.PyXel.My.Resources.Resources.run
+        Me.KryptonRibbonGroupButton4.TextLine1 = "Executer"
+        '
+        'KryptonRibbonGroupButton5
+        '
+        Me.KryptonRibbonGroupButton5.ImageLarge = Global.PyXel.My.Resources.Resources.console
+        Me.KryptonRibbonGroupButton5.ImageSmall = Global.PyXel.My.Resources.Resources.console
+        Me.KryptonRibbonGroupButton5.TextLine1 = "Console"
+        '
+        'KryptonRibbonTab2
+        '
+        Me.KryptonRibbonTab2.KeyTip = "D"
+        Me.KryptonRibbonTab2.Text = "Débogage"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "file.py"
+        '
+        'FastColoredTextBox1
+        '
+        Me.FastColoredTextBox1.AutoCompleteBracketsList = New Char() {Global.Microsoft.VisualBasic.ChrW(40), Global.Microsoft.VisualBasic.ChrW(41), Global.Microsoft.VisualBasic.ChrW(123), Global.Microsoft.VisualBasic.ChrW(125), Global.Microsoft.VisualBasic.ChrW(91), Global.Microsoft.VisualBasic.ChrW(93), Global.Microsoft.VisualBasic.ChrW(34), Global.Microsoft.VisualBasic.ChrW(34), Global.Microsoft.VisualBasic.ChrW(39), Global.Microsoft.VisualBasic.ChrW(39)}
+        Me.FastColoredTextBox1.AutoScrollMinSize = New System.Drawing.Size(27, 14)
+        Me.FastColoredTextBox1.BackBrush = Nothing
+        Me.FastColoredTextBox1.CharHeight = 14
+        Me.FastColoredTextBox1.CharWidth = 8
+        Me.FastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.FastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.FastColoredTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FastColoredTextBox1.Font = New System.Drawing.Font("Courier New", 9.75!)
+        Me.FastColoredTextBox1.IsReplaceMode = False
+        Me.FastColoredTextBox1.Location = New System.Drawing.Point(0, 115)
+        Me.FastColoredTextBox1.Name = "FastColoredTextBox1"
+        Me.FastColoredTextBox1.Paddings = New System.Windows.Forms.Padding(0)
+        Me.FastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.FastColoredTextBox1.ServiceColors = CType(resources.GetObject("FastColoredTextBox1.ServiceColors"), FastColoredTextBoxNS.ServiceColors)
+        Me.FastColoredTextBox1.Size = New System.Drawing.Size(1182, 418)
+        Me.FastColoredTextBox1.TabIndex = 1
+        Me.FastColoredTextBox1.Zoom = 100
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1182, 533)
+        Me.Controls.Add(Me.FastColoredTextBox1)
+        Me.Controls.Add(Me.KryptonRibbon1)
+        Me.Name = "Form1"
+        Me.Text = "PyXel - Sans Nom"
+        CType(Me.KryptonRibbon1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FastColoredTextBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents KryptonRibbon1 As ComponentFactory.Krypton.Ribbon.KryptonRibbon
+    Friend WithEvents KryptonContextMenuItem1 As ComponentFactory.Krypton.Toolkit.KryptonContextMenuItem
+    Friend WithEvents KryptonContextMenuItem2 As ComponentFactory.Krypton.Toolkit.KryptonContextMenuItem
+    Friend WithEvents KryptonContextMenuItem3 As ComponentFactory.Krypton.Toolkit.KryptonContextMenuItem
+    Friend WithEvents ButtonSpecAppMenu1 As ComponentFactory.Krypton.Ribbon.ButtonSpecAppMenu
+    Friend WithEvents ButtonSpecAppMenu2 As ComponentFactory.Krypton.Ribbon.ButtonSpecAppMenu
+    Friend WithEvents KryptonRibbonTab1 As ComponentFactory.Krypton.Ribbon.KryptonRibbonTab
+    Friend WithEvents KryptonRibbonGroup1 As ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup
+    Friend WithEvents KryptonRibbonGroupTriple1 As ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple
+    Friend WithEvents KryptonRibbonGroupButton1 As ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton
+    Friend WithEvents KryptonRibbonGroup2 As ComponentFactory.Krypton.Ribbon.KryptonRibbonGroup
+    Friend WithEvents KryptonRibbonGroupTriple2 As ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupTriple
+    Friend WithEvents KryptonRibbonGroupButton4 As ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton
+    Friend WithEvents KryptonRibbonGroupButton5 As ComponentFactory.Krypton.Ribbon.KryptonRibbonGroupButton
+    Friend WithEvents KryptonRibbonTab2 As ComponentFactory.Krypton.Ribbon.KryptonRibbonTab
+    Friend WithEvents KryptonRibbonQATButton1 As ComponentFactory.Krypton.Ribbon.KryptonRibbonQATButton
+    Friend WithEvents KryptonRibbonQATButton2 As ComponentFactory.Krypton.Ribbon.KryptonRibbonQATButton
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FastColoredTextBox1 As FastColoredTextBoxNS.FastColoredTextBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ PyXel/Form1.vb
@@ -0,0 +1,117 @@
+﻿Imports System.IO
+Imports System.Text.RegularExpressions
+Imports FastColoredTextBoxNS
+
+Public Class Form1
+
+    'Styles CodeEditor
+
+    Dim greenStyle As Style = New TextStyle(Brushes.Green, Brushes.White, FontStyle.Italic)
+    Dim blueStyle As Style = New TextStyle(Brushes.Blue, Brushes.White, FontStyle.Regular)
+    Dim orangeStyle As Style = New TextStyle(Brushes.Orange, Brushes.White, FontStyle.Regular)
+    Dim redStyle As Style = New TextStyle(Brushes.Red, Brushes.White, FontStyle.Bold)
+    Dim purpleStyle As Style = New TextStyle(Brushes.Purple, Brushes.White, FontStyle.Bold)
+
+    'Variables Fichiers
+    Dim isFileSet As Boolean = False
+    Dim fileName As String = "Sans Nom"
+    Dim isFileSaved As Boolean = True
+
+    Private Sub KryptonContextMenuItem2_Click(sender As Object, e As EventArgs) Handles KryptonContextMenuItem2.Click
+        If isFileSaved = False Then
+            Dim msg As String
+            Dim title As String
+            Dim style As MsgBoxStyle
+            msg = "Voulez-vous sauvegarder le fichier avant de continuer ?"   ' Define message.
+            style = MsgBoxStyle.YesNoCancel
+            title = "PyXel - Fichier non sauvegardé"
+            Dim result As MsgBoxResult = MsgBox(msg, style, title)
+            If result = MsgBoxResult.Yes Then
+                SaveFile()
+            ElseIf result = MsgBoxResult.Cancel Then
+                Exit Sub
+            End If
+        End If
+
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.Filter = "Fichiers Python|*.py"
+        openFileDialog1.Title = "Ouvrir un fichier Python"
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Dim sr As New System.IO.StreamReader(openFileDialog1.FileName)
+            FastColoredTextBox1.Text = sr.ReadToEnd
+            sr.Close()
+            isFileSet = True
+            fileName = openFileDialog1.FileName
+            Me.Text = "PyXel - " + fileName
+        End If
+    End Sub
+
+    Private Sub FastColoredTextBox1_TextChanged(sender As Object, e As TextChangedEventArgs) Handles FastColoredTextBox1.TextChanged
+        e.ChangedRange.ClearStyle(greenStyle)
+        e.ChangedRange.ClearStyle(orangeStyle)
+        e.ChangedRange.ClearStyle(blueStyle)
+        e.ChangedRange.ClearStyle(redStyle)
+        e.ChangedRange.ClearStyle(purpleStyle)
+        e.ChangedRange.SetStyle(greenStyle, "#.*$", RegexOptions.Multiline)
+        e.ChangedRange.SetStyle(blueStyle, "(print|input|if|else|for|in^t|range|while|try|except|catch|\(|\))")
+        e.ChangedRange.SetStyle(orangeStyle, "(int|float|string)")
+        e.ChangedRange.SetStyle(redStyle, "(def|import|as|from)")
+        e.ChangedRange.SetStyle(purpleStyle, "" + Chr(34) + "(.*?)" + Chr(34) + "")
+        If FastColoredTextBox1.Text.Length > 0 Then
+            Me.Text = "PyXel - " + fileName + "*"
+            isFileSaved = False
+        End If
+    End Sub
+
+
+
+    Public Async Sub SaveFile()
+        If isFileSet Then
+            Using outputFile As New StreamWriter(fileName)
+                Await outputFile.WriteAsync(FastColoredTextBox1.Text)
+            End Using
+            Me.Text = "PyXel - " + fileName
+        Else
+            Dim saveFileDialog As New SaveFileDialog
+            saveFileDialog.Filter = "Fichiers Python|*.py"
+            saveFileDialog.Title = "Enregistrer un fichier Python"
+            If saveFileDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                fileName = saveFileDialog.FileName
+                Using outputFile As New StreamWriter(fileName)
+                    Await outputFile.WriteAsync(FastColoredTextBox1.Text)
+                End Using
+            End If
+            Me.Text = "PyXel - " + fileName
+        End If
+    End Sub
+
+    Private Sub KryptonContextMenuItem3_Click(sender As Object, e As EventArgs) Handles KryptonContextMenuItem3.Click
+        SaveFile()
+    End Sub
+
+    Private Sub KryptonRibbonQATButton1_Click(sender As Object, e As EventArgs) Handles KryptonRibbonQATButton1.Click
+        SaveFile()
+    End Sub
+
+    Private Sub KryptonContextMenuItem1_Click(sender As Object, e As EventArgs) Handles KryptonContextMenuItem1.Click
+        If isFileSaved = False Then
+            Dim msg As String
+            Dim title As String
+            Dim style As MsgBoxStyle
+            msg = "Voulez-vous sauvegarder le fichier avant de continuer ?"   ' Define message.
+            style = MsgBoxStyle.YesNoCancel
+            title = "PyXel - Fichier non sauvegardé"
+            Dim result As MsgBoxResult = MsgBox(msg, style, title)
+            If result = MsgBoxResult.Yes Then
+                SaveFile()
+            ElseIf result = MsgBoxResult.Cancel Then
+                Exit Sub
+            End If
+        End If
+
+        isFileSet = False
+        fileName = "Sans Nom"
+        isFileSaved = True
+        FastColoredTextBox1.Text = ""
+    End Sub
+End Class
