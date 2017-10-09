f3b3f03e592ec07509eaef58cc38c641caea1752
amacz13 <amacz13@outlook.fr>
2017-10-09 06:53:49 +0200

    Fix: - About window was not centered & resized

--- PyXel/About.Designer.vb
+++ PyXel/About.Designer.vb
@@ -41,15 +41,15 @@ Partial Class About
         Me.KryptonPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.KryptonPanel1.Location = New System.Drawing.Point(0, 0)
         Me.KryptonPanel1.Name = "KryptonPanel1"
-        Me.KryptonPanel1.Size = New System.Drawing.Size(981, 399)
+        Me.KryptonPanel1.Size = New System.Drawing.Size(441, 395)
         Me.KryptonPanel1.TabIndex = 0
         '
         'KryptonHeader2
         '
         Me.KryptonHeader2.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.KryptonHeader2.Location = New System.Drawing.Point(0, 67)
+        Me.KryptonHeader2.Location = New System.Drawing.Point(0, 63)
         Me.KryptonHeader2.Name = "KryptonHeader2"
-        Me.KryptonHeader2.Size = New System.Drawing.Size(981, 31)
+        Me.KryptonHeader2.Size = New System.Drawing.Size(441, 31)
         Me.KryptonHeader2.TabIndex = 3
         Me.KryptonHeader2.Values.Description = ""
         Me.KryptonHeader2.Values.Heading = "Licence"
@@ -58,10 +58,10 @@ Partial Class About
         'KryptonRichTextBox1
         '
         Me.KryptonRichTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.KryptonRichTextBox1.Location = New System.Drawing.Point(0, 98)
+        Me.KryptonRichTextBox1.Location = New System.Drawing.Point(0, 94)
         Me.KryptonRichTextBox1.Name = "KryptonRichTextBox1"
         Me.KryptonRichTextBox1.ReadOnly = True
-        Me.KryptonRichTextBox1.Size = New System.Drawing.Size(981, 301)
+        Me.KryptonRichTextBox1.Size = New System.Drawing.Size(441, 301)
         Me.KryptonRichTextBox1.TabIndex = 2
         Me.KryptonRichTextBox1.Text = resources.GetString("KryptonRichTextBox1.Text")
         '
@@ -78,7 +78,7 @@ Partial Class About
         Me.KryptonHeader1.Dock = System.Windows.Forms.DockStyle.Top
         Me.KryptonHeader1.Location = New System.Drawing.Point(0, 0)
         Me.KryptonHeader1.Name = "KryptonHeader1"
-        Me.KryptonHeader1.Size = New System.Drawing.Size(981, 31)
+        Me.KryptonHeader1.Size = New System.Drawing.Size(441, 31)
         Me.KryptonHeader1.TabIndex = 0
         Me.KryptonHeader1.Values.Description = "InDev 0.0.1"
         Me.KryptonHeader1.Values.Heading = "PyXel IDE"
@@ -88,7 +88,7 @@ Partial Class About
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(981, 399)
+        Me.ClientSize = New System.Drawing.Size(441, 395)
         Me.Controls.Add(Me.KryptonPanel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
@@ -96,6 +96,7 @@ Partial Class About
         Me.Name = "About"
         Me.ShowIcon = False
         Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "A propos de PyXel IDE"
         CType(Me.KryptonPanel1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.KryptonPanel1.ResumeLayout(False)
